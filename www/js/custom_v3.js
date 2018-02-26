49575cb3f49b049d3ddf39ab7d8bcce45e7ce384
matheuscoelho <matheuscoelhoo321@gmail.com>
2018-02-26 11:07:30 -0300

    a
    a

--- www/js/custom_v3.js
+++ www/js/custom_v3.js
@@ -11,37 +11,6 @@ var link_pagseguro = 'https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/
 
 
 
-function getImage() {
- navigator.camera.getPicture(uploadPhoto, function(message) {
-    alert('get picture failed');
- }, {
- quality: 100,
- destinationType: navigator.camera.DestinationType.FILE_URI,
- sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
- });
-}
-
-function uploadPhoto(imageURI) {
- var options = new FileUploadOptions();
- options.fileKey = "file";
- options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
- options.mimeType = "image/jpeg";
- console.log(options.fileName);
- var params = new Object();
- params.value1 = "test";
- params.value2 = "param";
- options.params = params;
- options.chunkedMode = false;
-
-var ft = new FileTransfer();
- ft.upload(imageURI, "http://vipsio.com.br/admin/api/upload_fotos_api.php", function(result){
- console.log(JSON.stringify(result));
- }, function(error){
- console.log(JSON.stringify(error));
- }, options);
- }
-
-
 
 
 
@@ -1037,7 +1006,7 @@ $(document).ready(function () {
         });
 
         $(document).on('click', '#mr_camera', function () {
-           getImage();
+           
         });
 
         $(document).on('click', '#agendar_a', function () {
